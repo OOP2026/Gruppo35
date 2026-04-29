e396a2e9c51c06f982be992de79d41476e4af6eb
AlessandroMormone <ales.mormone@studenti.unina.it>
2026-04-29 15:34:55 +0200

    Update Utente.java

--- src/main/java/model/Utente.java
+++ src/main/java/model/Utente.java
@@ -1,17 +1,38 @@
 package model;
 
-import java.util.ArrayList;
-
-public class Utente {
-    private String login;
+//classe astratta così che non si creano oggetti di tipo utente, data la generalizzazione con studente e docente
+//inoltre definiamo gli attributi che verranno passati come attributi nelle classi figlie, senza doverli riscrivere
+public abstract class Utente {
+    private String nome;
+    private String cognome;
+    private String email;
     private String password;
 
-    public Utente(String login, String password) {
-        this.login = login;
+    public Utente(String nome, String cognome, String email, String password) {
+        this.nome = nome;
+        this.cognome = cognome;
+        this.email = email;
         this.password = password;
     }
 
-    public boolean login(String login, String password) {
-        return ( login.equals(this.login) && password.equals(this.password));
-    }
+    //get e set nel caso servissero
+    public String getNome() {
+        return nome; }
+    public void setNome(String nome) {
+        this.nome = nome; }
+
+    public String getCognome() {
+        return cognome; }
+    public void setCognome(String cognome) {
+        this.cognome = cognome; }
+
+    public String getEmail() {
+        return email; }
+    public void setEmail(String email) {
+        this.email = email; }
+
+    public String getPassword() {
+        return password; }
+    public void setPassword(String password) {
+        this.password = password; }
 }
