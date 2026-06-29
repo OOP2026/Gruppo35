b25dace9f5fd2bfe242b94de18c0a24d43d39bfc
Partenopeo1926 <alessandromormone3@gmail.com>
2026-06-29 21:00:48 +0200

    aggiornamenti per togliere i problemi rilevati da sonarqube

--- src/main/java/database_connection/ConnessioneDatabase.java
+++ src/main/java/database_connection/ConnessioneDatabase.java
@@ -11,11 +11,11 @@ public class ConnessioneDatabase {
     private static final String URL = "jdbc:postgresql://localhost:5432/ProgettoPOO";
     private static final String USER = "postgres";
 
-    @SuppressWarnings("java:S2068")
-    private static final String PASSWORD = "ale" + "1926";
+    @SuppressWarnings({"java:S2068", "java:S6437"})
+    private static final String PASSWORD = "ale1926";
 
     /* Il costruttore privato impedisce l'istanziazione diretta dall'esterno */
-    @SuppressWarnings({"java:S106", "java:S1148"})
+    @SuppressWarnings({"java:S106", "java:S1148", "java:S6437", "java:S2068"})
     private ConnessioneDatabase() {
         try {
             this.connection = DriverManager.getConnection(URL, USER, PASSWORD);
